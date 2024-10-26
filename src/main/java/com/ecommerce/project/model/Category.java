b24a971b22372dc728cd6fd3c7335c8d3907048c
belvip <pouadjeubelvi@gmail.com>
2024-10-26 16:22:22 +0100

    strategies Generation

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -2,13 +2,21 @@ package com.ecommerce.project.model;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "categories") // To map the database
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
+
+    // Default constructor
+    public Category() {
+
+    }
 
     public Long getCategoryId() {
 
