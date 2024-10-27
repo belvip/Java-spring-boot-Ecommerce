8bdf6978338884494017a5d2f5fd2286134892c4
belvip <pouadjeubelvi@gmail.com>
2024-10-27 07:38:31 +0100

    hibernate-validator

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -35,15 +35,8 @@ public class CategoryServiceImpl implements CategoryService  {
 
     @Override
     public String deleteCategory(Long categoryId) {
-
-       List<Category> categories = categoryRepository.findAll();
-        Category category = categories.stream().
-                filter(c -> c.getCategoryId().equals(categoryId)).
-                findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
-       /* if(category == null){
-            return "Category not found";
-        }
-        */
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() ->  new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
 
         categoryRepository.delete(category);
 
@@ -52,19 +45,14 @@ public class CategoryServiceImpl implements CategoryService  {
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
-        Optional<Category> optionalCategory = categories.stream().
-                filter(c -> c.getCategoryId().equals(categoryId)).
-                findFirst();
 
-        if(optionalCategory.isPresent()) {
-            Category existingCategory = optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            Category savedCategory = categoryRepository.save(existingCategory);
-            return savedCategory;
-        } else{
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-        }
+        Category savedCategory = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"));
+
+        category.setCategoryId(categoryId);
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
+
 
     }
 }
