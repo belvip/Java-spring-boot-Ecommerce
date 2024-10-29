00045eaeb8ceac80492c11a4ef7cda9285b5ea64
belvip <pouadjeubelvi@gmail.com>
2024-10-29 12:47:28 +0100

    Pageagle and PageRequest

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -18,16 +18,12 @@ public class CategoryController {
     @Autowired // Fill injection instead of CategoryController(CategoryService categoryService)
     private CategoryService categoryService;  // Create an object of service
 
-   @GetMapping("/echo")
-    public ResponseEntity<String> echoMessage(@RequestParam(name = "message") String message){
-       // public ResponseEntity<String> echoMessage(@RequestParam(name = "message", defaultValue = "Hello world") String message)
-        return new ResponseEntity<>("Echo message : " + message, HttpStatus.OK);
-    }
-
     // http://localhost:8080/api/public/categories
     @GetMapping("/public/categories")
-    public ResponseEntity<CategoryResponse> getAllCategories() {
-        CategoryResponse categoryResponse = categoryService.getAllCategories();
+    public ResponseEntity<CategoryResponse> getAllCategories(
+            @RequestParam(name = "pageNumber") Integer pageNumber,
+            @RequestParam(name = "pageSize") Integer pageSize) {
+        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber, pageSize);
         return new ResponseEntity<>(categoryResponse, HttpStatus.CREATED);
     }
 
